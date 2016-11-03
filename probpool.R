701b23ecb617d7fe92f2f0f84e9c58d1c224fb47
ChrKoenig <chr.koenig@outlook.com>
2016-11-03 14:01:04 +0100

    restructuring

--- probpool.R
+++ probpool.R
@@ -1,41 +1,43 @@
 library(methods)
 library(raster)
 
-# Class definition
-setClass("probpool",
-         slots = c(env.pool = "Raster",
-                   dist.pool = "Raster",
-                   bio.pool = "Raster",
-                   PSI.env = "Raster",
-                   PSI.dist = "Raster",
-                   PSI.bio = "Raster",
-                   PSI.total = "Raster"))
-
 # Constructor function
-probpool = function(env.pool = NA, dist.pool = NA, bio.pool = NA){
-  args = list(env.pool = env.pool, dist.pool = dist.pool, bio.pool = bio.pool)
-  # Check arguments
-  if(length(which(is.na(args))) == 3){stop("no probabilities provided")}
-  
-  # Check dimensions
-  arg.dims = data.frame(lapply(args[!is.na(args)], function(x) dim(x)[1:2]))
-  if(!all(apply(arg.dims, 1, function(x){length(unique(x)) == 1}))){stop("Dimension mismatch")}
-  
-  # fill 
-  args[is.na(args)] = 1
-  
-  #
-  PSI.env = sum(args$env.pool)
-  PSI.dist = sum(args$dist.pool)
-  PSI.bio = sum(args$bio.pool)
-  PSI.total = sum(PSI.env, PSI.dist, PSI.bio)
+probpool = function(env.pool = NULL, disp.pool = NULL, bio.pool = NULL){
   new("probpool", 
-      env.pool = args$env.pool, 
-      dist.pool = args$dist.pool, 
-      bio.pool = args$bio.pool,
-      PSI.env = PSI.env,
-      PSI.dist = PSI.dist,
-      PSI.bio = PSI.bio,
-      PSI.total = PSI.total
+      pools = list(env.pool = env.pool, disp.pool = disp.pool, bio.pool = bio.pool),
+      PSI = list(PSI.env = sum(env.pool), PSI.disp = sum(disp.pool), 
+                 PSI.bio = sum(bio.pool), PSI.total = sum(env.pool, disp.pool, bio.pool)),
+      PDF = list(),
+      CDF = list(),
+      slots = c("pools", "PSI", "PDF", "CDF")
   )
 }
+
+is.valid.probpool = function(object){
+  errors = character()
+  if(all(sapply(object@pools, is.null))){
+    errors = c(errors,"no probabilities provided")
+  }
+  # Check types
+  sapply(object@pools, function(pool){
+    if(!(extends(class(pool), "Raster") | is.matrix(pool) | is.null(pool))){
+      errors = c(errors, "Invalid pool type. Please provide a raster object or a matrix")
+    }
+  })
+  # Check dimensions
+  pool.dims = sapply(object@pools, dim)
+  if(length(unique(pool.dims[1,])) != 1 | length(unique(pool.dims[2,])) != 1){
+    errors = c(errors, "All pools need to have the same dimensions")
+  }
+  
+  return(ifelse(length(errors) == 0, TRUE, errors))
+}
+
+# Class definition
+setClass("probpool",
+         slots = c(pools = "list",
+                   PSI = "list",
+                   PDF = "list",
+                   CDF = "list",
+                   slots = "character"),
+         validity = is.valid.probpool)
