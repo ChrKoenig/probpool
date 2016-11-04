5dea875b0aab81518e15eaab01ccda485cbc97d6
Dirk Karger <dirk.karger@systbot.uzh.ch>
2016-11-04 09:52:07 +0100

    changes

--- sReg_envpool.R
+++ sReg_envpool.R
@@ -1,29 +1,35 @@
-probpool.default <- function(env.pool, bio.pool, dist.pool ...)
-{
-  
-  
-  env.pool <- env.pool
-  bio.pool <- bio.pool
-  dis.pool <- dis.pool
-  
-  phi.i    <- 
-  
-  y <- as.numeric(y)
-  est <- linmodEst(x, y)
-  est$fitted.values <- as.vector(x %*% est$coefficients)
-  est$residuals <- y - est$fitted.values
-  est$call <- match.call()
-  
-  
-  class(probpool) <- "probpool"
-  est
-}
+
+setMethod("plot", c("list"),
+          function(x, focalunit,...)
+            {
+              #if(is.vector(x$dist.pool)==TRUE && is.vector(x$env.pool==TRUE && is.vector(x$bio.pool==TRUE)))
+              if(length(focalunit)==2){
+              par(mfrow=c(3,3))
+              plot(x$dist.pool[focalunit[1],focalunit[2]], names(x$dist.pool),  ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
+              barplot(x$dist.pool.pdf[focalunit[1],focalunit[2]],  ylab = "Probabilities", xlab= "Species",, main = expression(paste(psi, "-dist")))
+              barplot(rbind(x$dist.pool.cdf[focalunit[1],focalunit[2]], 1-x$dist.pool.cdf[focalunit[1],focalunit[2]]), ylim=c(0, 1),, main = expression(paste(psi, "-dist")))
+             
+              plot(x$env.pool[focalunit[1],focalunit[2]], names(x$env.pool),  ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-env")))
+              barplot(x$env.pool.pdf[focalunit[1],focalunit[2]],  ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-env")))
+              barplot(rbind(x$env.pool.cdf[focalunit[1],focalunit[2]], 1-x$env.pool.cdf[focalunit[1],focalunit[2]]), ylim=c(0, 1), main = expression(paste(psi, "-env")))
+
+              plot(x$bio.pool[focalunit[1],focalunit[2]], names(x$bio.pool),  ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-bio")))
+              barplot(x$bio.pool.pdf[focalunit[1],focalunit[2]],  ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-bio")))
+              barplot(rbind(x$env.pool.cdf[focalunit[1],focalunit[2]], 1-x$env.pool.cdf[focalunit[1],focalunit[2]]), ylim=c(0, 1), main = expression(paste(psi, "-bio")))
+              }else{
+              par(mfrow=c(2,4))  
+              raster::plot(x$psi.dist, main=expression(paste(psi, "-dist")))
+              raster::plot(x$psi.env,  main=expression(paste(psi, "-env")))
+              raster::plot(x$psi.bio,  main=expression(paste(psi, "-bio")))
+              raster::plot(x$psi.bio*x$psi.dist*x$psi.env,  main=expression(paste(psi, "-dist+env+bio")))
+              raster::plot(x$psi.env*x$psi.dist,  main=expression(paste(psi, "-dist+env")))
+              raster::plot(x$psi.bio*x$psi.dist,  main=expression(paste(psi, "-dist+bio")))
+              raster::plot(x$psi.bio*x$psi.env,  main=expression(paste(psi, "-env+bio")))
+              
+              }
+          }
+)
 
 
 
-calc.phi.i  <- function (x)
-{
-  calc(calc(r, fun=function(x){x * 2})
-  
-}
 
