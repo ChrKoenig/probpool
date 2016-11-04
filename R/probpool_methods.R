bdf3bed0c6e1e4483903a0e0018496fc50766b25
Dirk Karger <dirk.karger@systbot.uzh.ch>
2016-11-04 12:10:54 +0100

    setMethods changes

--- R/probpool_methods.R
+++ R/probpool_methods.R
@@ -2,41 +2,55 @@
 setMethod("plot", c("probpool"),
           function(x, focalunit=FALSE,...)
           {
-            if(length(focalunit)==2){
+            if(length(focalunit)==2)
+              {
               par(mfrow=c(4,3))
-              focal<-cbind(focalunit[1],focalunit[2])
-              loc<-extract(x@pools$disp.pool,focal, cellnumbers=TRUE)
               
-              barplot(rev(sort(x@pools$disp.pool[loc[1]][1,])), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
-              barplot(dpoibin(seq(1,length(x@pools$disp.pool[focalunit[1],focalunit[2]][1,]),1), x@pools$disp.pool[focalunit[1],focalunit[2]][1,]), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
-              barplot(ppoibin(seq(1,length(x@pools$disp.pool[focalunit[1],focalunit[2]][1,]),1), x@pools$disp.pool[focalunit[1],focalunit[2]][1,]), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
+              if(is.null(x@pools$env.pool)==FALSE)
+              {
+                plotPoolProbs(x,"env.pool",focalunit)
+                plotPoolPDF(x,"env.pool",focalunit)
+                plotPoolCDF(x,"env.pool",focalunit)
+              }
               
-               
-              barplot(rev(sort(x@pools$env.pool[loc[1]][1,])), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-env")))
-              barplot(dpoibin(seq(1,length(x@pools$env.pool[loc[1]][1,]),1), x@pools$env.pool[focalunit[1],focalunit[2]][1,]), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
-              barplot(ppoibin(seq(1,length(x@pools$env.pool[loc[1]][1,]),1), x@pools$env.pool[focalunit[1],focalunit[2]][1,]), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
+              if(is.null(x@pools$disp.pool)==FALSE)
+              {
+                plotPoolProbs(x,"disp.pool",focalunit)
+                plotPoolPDF(x,"disp.pool",focalunit)
+                plotPoolCDF(x,"disp.pool",focalunit)
+              }
               
-                 
-              barplot(rev(sort(x@pools$bio.pool[loc[1]][1,])), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-bio")))
-              barplot(dpoibin(seq(1,length(x@pools$bio.pool[loc[1]][1,]),1), x@pools$bio.pool[focalunit[1],focalunit[2]][1,]), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
-              barplot(ppoibin(seq(1,length(x@pools$bio.pool[loc[1]][1,]),1), x@pools$bio.pool[focalunit[1],focalunit[2]][1,]), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
+              if(is.null(x@pools$bio.pool)==FALSE)
+              {
+                plotPoolProbs(x,"bio.pool",focalunit)
+                plotPoolPDF(x,"bio.pool",focalunit)
+                plotPoolCDF(x,"bio.pool",focalunit)
+              }
               
-             
-              barplot(rev(sort(x@pools$bio.pool[loc[1]][1,]*x@pools$disp.pool[focalunit[1],focalunit[2]][1,]*x@pools$env.pool[focalunit[1],focalunit[2]][1,])), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "disp*env*bio")))
-              barplot(dpoibin(seq(1,length(x@pools$bio.pool[loc[1]][1,]),1), x@pools$bio.pool[loc[1]][1,]*x@pools$disp.pool[loc[1]][1,]*x@pools$env.pool[loc[1]][1,]), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
-              barplot(ppoibin(seq(1,length(x@pools$bio.pool[loc[1]][1,]),1), x@pools$bio.pool[loc[1]][1,]*x@pools$disp.pool[loc[1]][1,]*x@pools$env.pool[loc[1]][1,]), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
-              
-              fullpool.rst<-x@pools$disp.pool*x@pools$env.pool*x@pools$bio.pool
-              fullpool    <-raster::calc(fullpool.rst,fun=sum)
+              #fullpool.rst<-x@pools$disp.pool*x@pools$env.pool*x@pools$bio.pool
+              #fullpool    <-raster::calc(fullpool.rst,fun=sum)
               
               }else{
-              par(mfrow=c(2,4))  
-              raster::plot(x@PSI$PSI.disp, main=expression(paste(psi, "-disp")))
-              raster::plot(x@PSI$PSI.env,  main=expression(paste(psi, "-env")))
-              raster::plot(x@PSI$PSI.bio,  main=expression(paste(psi, "-bio")))
-              raster::plot(fullpool,  main=expression(paste(psi, "-disp+env+bio")))
-              
-              
+                
+              par(mfrow=c(2,4))    
+                
+                if(is.null(x@PSI$bio.pool)==FALSE)  
+                {
+                  plotRasterPool(x,"bio.pool")
+                }
+                
+                if(is.null(x@PSI$env.pool)==FALSE)  
+                {
+                  plotRasterPool(x,"env.pool")
+                }
+                
+                if(is.null(x@PSI$bio.pool)==FALSE)  
+                {
+                  plotRasterPool(x,"disp.pool")
+                }
+
+              #raster::plot(fullpool,  main=expression(paste(psi, "-disp+env+bio")))
+
             }
           }
 )
@@ -49,13 +63,21 @@ plotPoolProbs<-function(probpool,pool,focalunit)
   barplot(rev(sort(probpool@pools[[pool]][loc[1]][1,])), ylim=c(0,1), ylab = "Probabilities", xlab= "Species", main = pool)
 }
 
+# Function to plot pool probability density function
 plotPoolPDF<-function(probpool,pool,focalunit)
 {
   focal<-cbind(focalunit[1],focalunit[2])
   loc<-extract(probpool@pools[[pool]],focal, cellnumbers=TRUE)
-  barplot(dpoibin(seq(1,length(probpool@pools[[pool]][focalunit[1],focalunit[2]][1,]),1), probpool@pools[[pool]][focalunit[1],focalunit[2]][1,]), ylab = "Probabilities", xlab= "Species", main = expression(paste(psi, "-dist")))
+  barplot(dpoibin(seq(1,length(probpool@pools[[pool]][loc[1]][1,]),1), probpool@pools[[pool]][loc[1]][1,]), names.arg=1:length(probpool@pools[[pool]][loc[1]][1,]), ylab = "Probabilities", xlab= "Species", main = pool)
 }
 
+# Function to plot pool cumulative density function
+plotPoolCDF<-function(probpool,pool,focalunit)
+{
+  focal<-cbind(focalunit[1],focalunit[2])
+  loc<-extract(probpool@pools[[pool]],focal, cellnumbers=TRUE)
+  barplot(ppoibin(seq(1,length(probpool@pools[[pool]][loc[1]][1,]),1), probpool@pools[[pool]][loc[1]][1,]), ylim=c(0,1), names.arg=1:length(probpool@pools[[pool]][loc[1]][1,]), ylab = "Probabilities", xlab= "Species", main = pool)
+}
 
 # Function to plot pool as a raster
 plotRasterPool<-function(probpool,pool)
